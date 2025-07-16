d3ebc9d75e90ff7615900e8e1ae4c8b8aa27d419
Andre Michels <YOUR_EMAIL>
2025-07-15 22:09:17 -0300

    feat(pages): Convert static HTML to initial React page components

---
+++ src/app/camarote/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function Page() {
+  return (
+    <main className="container mx-auto px-4">
+      <h1 className="text-2xl font-bold mb-4"></h1>
+      <div className="content">
+        window.__WEBFLOW_CURRENCY_SETTINGS = {"currencyCode":"USD","symbol":"$","decimal":".","fractionDigits":2,"group":",","template":"{{wf {\"path\":\"symbol\",\"type\":\"PlainText\"} }} {{wf {\"path\":\"amount\",\"type\":\"CommercePrice\"} }} {{wf {\"path\":\"currencyCode\",\"type\":\"PlainText\"} }}","hideDecimalForWholeNumbers":false};CAMAROTECOMO CHEGARPROGRAMAçÃOSUSTENTABILIDADECONTATOBAHIACAMAROTEUma experiência mágica no coração do Carnaval de Salvador! No camarote VIVA BAHIA, a festa ganha um novo significado, mantendo toda a tradição da cultura baiana.Uma experiência mágica no coração do Carnaval de Salvador! No camarote VIVA BAHIA, a festa ganha um novo significado, mantendo toda a tradição da cultura baiana.
+  [data-wf-bgvideo-fallback-img] {
+    display: none;
+  }
+  @media (prefers-reduced-motion: reduce) {
+    [data-wf-bgvideo-fallback-img] {
+      position: absolute;
+      z-index: -100;
+      display: inline-block;
+      height: 100%;
+      width: 100%;
+      object-fit: cover;
+    }
+  }Uma ocupação de alegria no salão do Edifício Oceania, um clássico da arquitetura da cidade, que reabre com todo o conforto para oferecer diversão com a melhor vista do Farol da Barra, e ecoar axé na avenida em um pôr do sol deslumbrante.O VIVA BAHIA é mais que um camarote: é uma celebração da vida, da cultura e da alegria em uma experiência inesquecível que só o Carnaval de Salvador sabe proporcionar.27/02 a 04/03 | 15h - 01hCircuito Barra-OndinaEdifício Oceania - Em frente ao Farol da BarraSalvador - BAO VIVA BAHIA é mais que um camarote: é uma celebração da vida, da cultura e da alegria em uma experiência inesquecível que só o Carnaval de Salvador sabe proporcionar.27/02 a 04/03 | 15h - 01hCircuito Barra-OndinaEdifício Oceania - Em frente ao Farol da BarraSalvador - BAPATROCÍNIOTODOS OS DIREITOS RESERVADOS 2025D&amp;A MARKETING E EVENTOS LTDA - CNPJ: 27.928.742/0001-38
+      </div>
+    </main>
+  );
+}
